b5a902be02bb4b9eea0bb970a3b203428ae48392
Rémi Mondenx <remi.mondenx@grenoble-inp.org>
2018-11-01 16:26:07 +0100

    feuille de chene

--- src/Common/TopBar.js
+++ src/Common/TopBar.js
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+import logo from '../images/logos/chene.png'
+
+import { withStyles } from '@material-ui/core'
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import { Link } from 'react-router-dom'
-
 import { Button } from '@material-ui/core';
 
-const TopBar = ({items}) => (
-  <AppBar position='sticky'>
-    <Toolbar>
-      <Link to='/' style={{textDecoration: 'none'}}>
-        <Typography variant='title' color='inherit'>
-          Aux Tauzins
-        </Typography>
-      </Link>
-      {items.map(item => 
-        <Button href={item.href}>
-            {item.title}
-        </Button>
-      )}
-    </Toolbar>
-  </AppBar>
-)
+const styles = {
+    appBar: {
+        height: '60px',
+        paddingLeft: '120px'
+    },
+    img: {
+        position: 'absolute',
+        top: '10px',
+        left: '-110px',
+        height: '75px',
+    },
+}
 
-export default TopBar;
+class TopBar extends Component {
+    render() {
+        const { items, classes } = this.props;
+        return (
+            <AppBar className={classes.appBar} position='sticky'>
+                <Toolbar>
+                    <Link to='/' style={{textDecoration: 'none'}}>
+                        <img className={classes.img} src={logo} alt='logo' />
+                    </Link>
+                    {items.map(item => 
+                        <Button href={item.href}>
+                            {item.title}
+                        </Button>
+                    )}
+                </Toolbar>
+            </AppBar>
+        )
+    }
+}
+
+export default withStyles(styles)(TopBar);
