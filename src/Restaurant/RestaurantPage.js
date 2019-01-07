cb8a81f9515c3438a11e00a079dd0d6d557f2fa4
Rémi Mondenx <remi.mondenx@grenoble-inp.org>
2019-01-07 12:24:16 +0100

    resize images first layer for optim

--- src/Restaurant/RestaurantPage.js
+++ src/Restaurant/RestaurantPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import restaurant from '../images/main/restaurant_dark.jpg';
+import restaurant from '../images/main/restaurant.jpg';
 import FirstLayer from '../Common/FirstLayer';
 import MenusLayer from './MenusLayer';
 import LaCarteLayer from './LaCarteLayer';
