1ae9363d8d066a2779ab06c8d71510b14351373d
Rémi Mondenx <33282393+Mondenx614@users.noreply.github.com>
2020-12-15 10:00:28 +0100

    xmas

--- src/Common/FirstLayer.js
+++ src/Common/FirstLayer.js
@@ -244,7 +244,7 @@ class FirstLayer extends Component {
                     stdTypingDelay={0}
                     onTypingDone={() => setTimeout(this.done, 3000)}
                   >
-                    Découvrez notre menu à emporter à l'occasion du 11 Novembre. Contactez nous au 05 58 98 60 22.
+                    Nos menus du 25 décembre et 1er janvier vous sont proposés à emporter ! N'hésitez pas à nous contacter au 05 58 98 60 22. A très vite nous l'espérons !
                   </Typist>
                 )}
               </Typography>
@@ -254,7 +254,7 @@ class FirstLayer extends Component {
               <div className={classes.infoEltMobile}>
                 <div class="marquee">
                   <span>
-                    Découvrez notre menu à emporter à l'occasion du 11 Novembre. Contactez nous au 05 58 98 60 22.
+                    Nos menus du 25 décembre et 1er janvier vous sont proposés à emporter ! N'hésitez pas à nous contacter au 05 58 98 60 22. A très vite nous l'espérons !
                   </span>
                 </div>
               </div>
